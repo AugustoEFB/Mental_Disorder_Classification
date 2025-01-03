fa870e8e494820cc89db3e4b144cc3a581d613f1
Augusto <fncgtc96c05z605v@studenti.unical.it>
2025-01-03 21:42:13 +0100

    Initial commit

---
+++ my-new-project/my-cpp-project/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.h"
+
+int main() {
+    std::cout << "Welcome to my C++ project!" << std::endl;
+    
+    // Call a utility function (example)
+    utilityFunction();
+
+    return 0;
+}
---
+++ my-new-project/my-cpp-project/src/utils.cpp
@@ -0,0 +1,12 @@
+#include "utils.h"
+
+void utilityFunction1() {
+    // Implementation of utility function 1
+}
+
+int utilityFunction2(int a, int b) {
+    // Implementation of utility function 2
+    return a + b;
+}
+
+// Add more utility functions as needed.
